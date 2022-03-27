4ef2eaed58a5a8ed603eb800aeaf11b617e4eec8
ruucm <ruucm.a@gmail.com>
2022-03-27 13:58:21 +0900

    update Butotn with styled-components

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
 import styles from "./Button.modules.css";
+import styled from "styled-components";
+
+const Box = styled.span`
+  background: red;
+`;
 
 export function Button({ title = "Title" }) {
-  return <button className={styles.button}>{title}</button>;
+  return (
+    <button className={styles.button}>
+      {title}
+      <Box>Box</Box>
+    </button>
+  );
 }
