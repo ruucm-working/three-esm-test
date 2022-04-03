ddf38407cd2601958c828035e507eeac17d2632a
ruucm <ruucm.a@gmail.com>
2022-04-03 12:26:20 +0900

    useFiber + ThreeBox

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { Button } from "./Button";
 export { Battery } from "./Battery";
 export { MotionButton } from "./MotionButton";
 export { ThreeBox } from "./ThreeBox";
+export { FiberCanvas, FiberContextProvider } from "./useFiber";
